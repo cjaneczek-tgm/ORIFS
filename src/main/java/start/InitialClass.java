5e211c521e68b7af23175f3ef6d121fec9d57763
Chris <cjaneczek@student.tgm.ac.at>
2015-03-06 13:25:12 +0000

    Initial Commit

---
+++ src/main/java/start/InitialClass.java
@@ -0,0 +1,18 @@
+package start;
+
+import org.apache.log4j.Logger;
+
+/**
+ * The initial class for the "Orif" project
+ * @author Christian Janeczek
+ * @version 06/03/2015
+ */
+public class InitialClass {
+
+    private static Logger logger = Logger.getLogger(InitialClass.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Initial message...");
+    }
+}
